5c107f6f5b08cc9836e57b96b9baa327f798867e
unknown <davaladinesh34@gmail.com>
2026-01-14 14:10:32 +0530

    added analytics

---
+++ src/main/java/com/example/weather/controller/AnalyticsController.java
@@ -0,0 +1,37 @@
+package com.example.weather.controller;
+
+import com.example.weather.service.AnalyticsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/api/analytics")
+public class AnalyticsController {
+    @Autowired
+    private AnalyticsService analyticsService;
+    @GetMapping("/stats")
+    public ResponseEntity<Map<String, Object>> getStats() {
+        Map<String, Object> stats = new HashMap<>();
+
+        stats.put("totalHits", analyticsService.getTotalHits());
+        stats.put("uniqueVisitors", analyticsService.getUniqueVisitors());
+        stats.put("topCities", analyticsService.getTopCities());
+        stats.put("upTime",calculateUptime());
+        return ResponseEntity.ok(stats);
+    }
+    private String calculateUptime() {
+        Duration uptime = Duration.between(analyticsService.getStartTime(), LocalDateTime.now());
+        long hours =  uptime.toHours();
+        long minutes =  uptime.toMinutes()%60;
+        return String.format("%02d:%02d", hours, minutes);
+    }
+}
